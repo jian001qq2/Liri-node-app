386842a9ccb950e1a93ac0461d2e3514bcdbd31a
Jianhong <stjianhweng@gamil.com>
2018-10-30 15:17:03 -0400

    codes working

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
 # Liri-node-app
+ 
+
+## Overview
+This is an app called LIRI. LIRI is a _Language_ Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives user back with data. LIRI will search Spotify for songs, Bands in Town for concerts, and OMDB for movies.Then it will return and display the data it searched.
+
+## General Description How to Use This App
+As mentioned that LIRI will search Spotify for songs, Bands in Town for concerts,and OMDB for movies. The way to use it is pretty simple. 
+1. you starts by download this app then open Terminal(Mac) or Gitbash(windows) in your computer.
+2. next, you use the git commmands to enter to the directory of the files of this app then hit npm i to intall all the packages.
+3. congraduations, you are ready to use this app.
+4. then in the command line. enter the following syntax:
+"node  (one of the 4 commands)  (the name you want to search)" then hit enter
+Note: the 4 commmands are :  
+* `concert-this`
+* `spotify-this-song`
+* `movie-this`
+* `do-what-it-says`
+
+## Submission Guide
+
+Make sure you use the normal GitHub. Because this is a CLI App, there will be no need to deploy it to Heroku. This time, though, you need to include screenshots, a gif, and/or a video showing us that you got the app working with no bugs. You can include these screenshots or a link to a video in a `README.md` file.
+
+* Include screenshots (or a video) of typical user flows through your application (for the customer and if relevant the manager/supervisor). This includes views of the prompts and the responses after their selection (for the different selection options).
+
+* Include any other screenshots you deem necessary to help someone who has never been introduced to your application understand the purpose and function of it. This is how you will communicate to potential employers/other developers in the future what you built and why, and to show how it works. 
+
+* Because screenshots (and well-written READMEs) are extremely important in the context of GitHub, this will be part of the grading.
+
+If you haven't written a markdown file yet, [click here for a rundown](https://guides.github.com/features/mastering-markdown/), or just take a look at the raw file of these instructions.
+ 
+ 
+ 
+ 
+9. Make it so liri.js can take in one of the following commands:
+
+   * `concert-this`
+
+   * `spotify-this-song`
+
+   * `movie-this`
+
+   * `do-what-it-says`
+
+### What Each Command Should Do
+
+1. `node liri.js concert-this <artist/band name here>`
+
+   * This will search the Bands in Town Artist Events API (`"https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"`) for an artist and render the following information about each event to the terminal:
+
+     * Name of the venue
+
+     * Venue location
+
+     * Date of the Event (use moment to format this as "MM/DD/YYYY")
+
+2. `node liri.js spotify-this-song '<song name here>'`
+
+   * This will show the following information about the song in your terminal/bash window
+
+     * Artist(s)
+
+     * The song's name
+
+     * A preview link of the song from Spotify
+
+     * The album that the song is from
+
+   * If no song is provided then your program will default to "The Sign" by Ace of Base.
+ 
+
+3. `node liri.js movie-this '<movie name here>'`
+
+   * This will output the following information to your terminal/bash window:
+
+     ```
+       * Title of the movie.
+       * Year the movie came out.
+       * IMDB Rating of the movie.
+       * Rotten Tomatoes Rating of the movie.
+       * Country where the movie was produced.
+       * Language of the movie.
+       * Plot of the movie.
+       * Actors in the movie.
+     ```
+
+   * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
+
+  
+4. `node liri.js do-what-it-says`
+
+   * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+
+     * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
+
+     * Edit the text in random.txt to test out the feature for movie-this and concert-this.
+ 
+
+### Create a README.md
+
+Add a `README.md` to your repository describing the project. Here are some resources for creating your `README.md`. Here are some resources to help you along the way:
+
+* [About READMEs](https://help.github.com/articles/about-readmes/)
+
+* [Mastering Markdown](https://guides.github.com/features/mastering-markdown/)
+
+- - -
---
+++ liri.js
@@ -0,0 +1,167 @@
+//code to read and set any environment variables with the dotenv package
+require("dotenv").config();
+//declare avariables to require the keys
+var keys=require('./keys.js')
+//variables for require the packages and imports
+var Spotify = require('node-spotify-api');
+var request=require('request');
+var moment = require("moment");
+var request=require('request');
+var fs = require("fs");
+
+//genral set up
+var action=process.argv[2];
+var nameInput=process.argv[3];
+//create a function to handle different conmands
+function goLiri() {
+switch(action){
+  case('concert-this'):
+  cooncertThis();
+  ;
+  break;
+  case('spotify-this-song'):
+  spotifyThisSong()
+  ;break;
+  case('movie-this'):
+  movie();
+  ;break;
+  case('do-what-it-says'):
+  justDoIt()
+  break;
+  default:
+  console.log("\n Error, Please enter a valid command as required in this app")
+}
+}
+//area for the spotify-this-song
+/*//first declare variable to capture the name of the songs if unable then just use the name in the general
+it will output the following:
+//artists, song's name, preview link of the song from spotify, album of the song, 
+//the default song is The sign by Ace of Base.
+ */
+//issue item that not working
+var spotify = new Spotify(keys.spotify);
+
+function spotifyThisSong(){
+  //To declare a variable that handle two condition if true, grab the naem, else the sign
+  var song =nameInput || "The Sign";
+  spotify.search({ 
+    type: 'track', query: song 
+}, function (err, data){
+  if (err) {
+    return console.log("Error Code: " + err);
+
+  };
+    var songs = data.tracks.items;
+    for (let i = 0; i < songs.length; i++) {
+      console.log("\nResult "+(i+1));
+      console.log("Artist: " + songs[i].artists[0].name);
+      console.log("Song: " + songs[i].name);
+      console.log("Song Preview: " + songs[i].preview_url);
+      console.log("Album: " + songs[i].album.name);
+      console.log("\n+++++++++++++++++++++++end of search result " +(i+1)+"+++++++++++++++++++++++++++++++++++++++++++");
+  }
+  })
+}
+//end of the function spotify
+
+//area for Bands in Town
+/**coniditon for concert-this
+ * to check if the user enter the name for search, if he/she forgot to enter, then log a message to remind.
+ * declare variable to capture the name of artists/band
+ * //search the Bands in town API to printInfo the following info
+ * //output: name of hte venue, venue location, date of the event(use moment to format this as mm/dd/yyyy)
+ */
+function cooncertThis(){
+  //first check if the user enter the name for search
+    if (nameInput=== undefined) {
+        console.log("Error: You have to enter the name for a band or artist!");
+        return;
+    } else {
+      //perform the search
+      var artist=nameInput;
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=e321c5df3b581205409373ce3eb9c448";
+
+    request(queryUrl, function (error, response, body) {
+       //check to see there have any error(s)
+        if (!error && response.statusCode === 200) {
+        //declare a variable to make it hold the search result
+            var data = JSON.parse(body);
+            //break line for the top of the data
+            console.log("\n ++++++++++++++Start Data++++++++++++++++++++++++++++")
+            console.log("\nThese are upcoming concerts by: " + artist);
+            for (var i = 0; i < data.length; i++) {
+                var date = data[i].datetime;
+                date = moment(date).format("MM/DD/YYYY");
+                console.log("\nDate: " + date)
+                console.log("Venue: " + data[i].venue.name);
+                if (data[i].venue.region == "") {
+                    console.log("Location: " + data[i].venue.city + ", " + data[i].venue.country);
+                } else {
+                    console.log("Location: " + data[i].venue.city + ", " + data[i].venue.region + ", " + data[i].venue.country);
+                };
+            }
+            //break line for the end of data
+            console.log("\n++++++++++++++++++End data++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+        }
+    })
+};
+
+}
+
+//area for the movie
+//declare the url and a variable to caputure the user input for the movie name
+//request the site if is ok without error
+//output: Title, release year, IMDB rating, Rotten Tommatoes rating
+//Country of prodced , language, plot, actors
+// if no moive name input, then output data for the Mr.Nobody
+function movie(){
+var movieName=nameInput || "Mr.Nobody";
+var rottenTomatoRating, imdbRating;
+var movieQueryUrl="http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";;
+request(movieQueryUrl,function(error, response, body) {
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+    //declare a variable to store the result result
+    var data=JSON.parse(body);
+    //Make a if statement to obtain values of  matching rating sources as required
+for (var i = 0; i < data.Ratings.length; i++) {
+  if (data.Ratings[i].Source === "Rotten Tomatoes") {
+    rottenTomatoRating = data.Ratings[i].Value;
+  }
+  if (data.Ratings[i].Source === "Internet Movie Database") {
+      imdbRating = data.Ratings[i].Value;
+  }
+}
+console.log("\n ++++++++++++++++++++Moive Ouput Start+++++++++++++++++++++++++++++++++++++++++++")
+console.log("\nMovie Title: "+ data.Title);
+console.log("Movie Released in: "+ data.Year);
+console.log("IMDB Rating is: "+ imdbRating);
+console.log("Rotten Tomatoes Rating is: "+ rottenTomatoRating);
+console.log("Country: "+ data.Country);
+console.log("Available Languages: "+ data.Language);
+console.log("Plot: "+ data.Plot);
+console.log("Actors: "+ data.Actors);
+console.log("\n+++++++++++++++++++Moive Output End++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+} else{
+  console.log("Sorry, we have an issue.")
+}
+  });//end of irequest
+};//end of moive
+
+//area of the do what it says
+/** using fs pakages to take text inside the random.txt, it will 
+ * call one of the commands. right now as default, it runs on spotify-this-song
+ * try to edit the text in the random.txt to try out other commaonds, or to find a way to make a random varible to choose 
+ */
+var justDoIt = function () {
+  fs.readFile("random.txt", "utf8", function (err, data) {
+      if (err) {
+          return console.log(err);
+      }
+      var fileData = data.split(",")
+      action=fileData[0];
+      nameInput = fileData[1];
+      goLiri();
+  });
+};
+goLiri();
